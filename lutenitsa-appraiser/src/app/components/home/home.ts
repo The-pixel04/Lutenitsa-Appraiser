e5da300419830022d5aabe05bedd867e89e22ca8
The-pixel04 <krsyrdyrd@gmail.com>
2025-07-08 12:50:09 +0300

    Add slideshow component to home page

--- lutenitsa-appraiser/src/app/components/home/home.ts
+++ lutenitsa-appraiser/src/app/components/home/home.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Slideshow } from "../slideshow/slideshow";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Slideshow],
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  slideshowFinished = false;
 
+  onSlideshowComplete() {
+    this.slideshowFinished = true;
+  }
 }
---
+++ lutenitsa-appraiser/src/app/components/slideshow/slideshow.ts
@@ -0,0 +1,58 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './slideshow.html',
+  styleUrl: './slideshow.css'
+})
+export class Slideshow implements OnInit {
+  @Output() slideshowComplete = new EventEmitter<void>();
+  slides = [
+    'https://foodmag.eu/image/cache/catalog/ProductPictures/MlechniProdukti/KashkavaliSirene/sireneOsmonweb-700x700.jpg',
+    'https://cdncloudcart.com/16372/products/images/42601/cuski-cerveni-kalifornia-1-kg-image_6585e8a32a599_800x800.jpeg',
+    'https://cdncloudcart.com/16372/products/images/41931/patladzan-1-kg-image_6585e60368221_1920x1920.jpeg',
+    'https://cdncloudcart.com/16372/products/images/41797/cerveni-domati-1-kg-image_6585c6b5f001b_1920x1920.jpeg'
+  ];
+
+  currentIndex = 0;
+  transitionSpeed = 300;
+  isActive = true;
+  private intervalId: any;
+
+  ngOnInit() {
+    this.startSlideshow();
+  }
+
+  startSlideshow() {
+    this.intervalId = setInterval(() => {
+      this.goToNextSlide();
+    }, this.transitionSpeed);
+  }
+
+  goToNextSlide() {
+    this.currentIndex++;
+
+    if (this.currentIndex >= this.slides.length) {
+      this.stopSlideshow();
+    }
+  }
+
+  stopSlideshow() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    this.slideshowComplete.emit()
+  }
+
+  onMouseEnter() {
+    this.stopSlideshow();
+  }
+
+  onMouseLeave() {
+    if (this.currentIndex < this.slides.length - 1) {
+      this.startSlideshow();
+    }
+  }
+}
