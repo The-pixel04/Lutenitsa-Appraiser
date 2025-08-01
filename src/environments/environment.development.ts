18e7dcee69e8b50c27949379f19ea73ae414c0ef
Kristian Yordanov <krsyrdyrd@gmail.com>
2025-08-01 11:25:59 +0300

    Refactor the app
    Change folders directories

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production:false,
+    apiUrl:'https://plvxnyvvvctelzhqfryq.supabase.co',
+    apiKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsdnhueXZ2dmN0ZWx6aHFmcnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2ODI5NDksImV4cCI6MjA2ODI1ODk0OX0.hpcucTxPSkoIXOpZbpUKaNCpRo6UF9td6un_tIZpXaw'
+};
